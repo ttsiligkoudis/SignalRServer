44f63f277c5fcc14a6bf820e505e57ca2fa68299
ttsiligkoudis <themtsil@gmail.com>
2023-02-03 10:18:59 +0200

    added more endpoints

--- SignalRServer/Hubs/ChatHub.cs
+++ SignalRServer/Hubs/ChatHub.cs
@@ -8,5 +8,30 @@ namespace SignalRServer.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        public async Task SendMessageToRoom(string user, string message, string connectionID)
+        {
+            await Clients.All.SendAsync("ReceiveMessageFromRoom", user, message, connectionID);
+        }
+
+        public async Task CheckConnectionID(string connectionID)
+        {
+            await Clients.All.SendAsync("CheckIfConnectionMatch", connectionID);
+        }
+
+        public async Task CheckConnectionIDAnswer(string connectionID, string word, string language)
+        {
+            await Clients.All.SendAsync("CheckIfConnectionMatchAnswer", connectionID, word, language);
+        }
+
+        public async Task SendAlphabetLetter(string connectionID, string letter)
+        {
+            await Clients.All.SendAsync("ReceiveAlphabetLetter", connectionID, letter);
+        }
+
+        public async Task SendResetGame(string connectionID, string word)
+        {
+            await Clients.All.SendAsync("ResetGame", connectionID, word);
+        }
     }
 }
